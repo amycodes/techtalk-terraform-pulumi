7044052f851957245485a093b7b03fa56bbcc530
Amy Negrette <anegrette@digitalocean.com>
2022-07-27 00:06:46 -0500

    Terraform Configuration

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+variable "do_token" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "techtalk-terraform-droplet" {
+  image  = "ubuntu-22-04-x64"
+  name   = "techtalk-terraform-droplet"
+  region = "sfo3"
+  size   = "s-4vcpu-8gb"
+}
+
+resource "digitalocean_firewall" "techtalk-terraform-firewall" {
+  name = "techtalk-terraform-firewall"
+
+  droplet_ids = [digitalocean_droplet.techtalk-terraform-droplet.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
